fdb2494e92445cfeafdb5b6255a1eb5087912a62
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-06 14:37:43 -0500

    created index file with error reporting

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Oleg Rovinskiy
+ * @date 1/6/2020
+ * @url https://github.com/orovinskiy/pp1.git
+ * Pair Programing 1
+ */
+
+    //error reporting
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
